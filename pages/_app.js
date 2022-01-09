8e6895f57b8748585247cb3a4d0bfe7e226b8315
Eugene <gzkgt11@gmail.com>
2022-01-09 15:41:33 +0800

    add Matemask auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import "regenerator-runtime/runtime";
+import "../styles/globals.css";
+import { ThirdwebProvider } from "@3rdweb/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // Ethereum chain ID
+  const supportedChainIds = [4];
+
+  // Support only Matemask
+  const connectors = {
+    injected: {},
+  };
+
+  return (
+    <ThirdwebProvider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <Component {...pageProps} />{" "}
+    </ThirdwebProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
